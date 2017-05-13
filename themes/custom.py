c53cdf534892e136daa94f00e09b711a037ae6f3
HATTA, Shozo <hachi8833@gmail.com>
2017-05-13 18:30:49 +0900

    Update color

--- themes/custom.py
+++ themes/custom.py
@@ -3,11 +3,11 @@ class DefaultColor:
     This class should have the default colors for every segment.
     Please test every new segment with this theme first.
     """
-    USERNAME_FG = 231
+    USERNAME_FG = 15
     USERNAME_BG = 240
     USERNAME_ROOT_BG = 124
 
-    HOSTNAME_FG = 231
+    HOSTNAME_FG = 15
     HOSTNAME_BG = 238
 
     HOME_SPECIAL_DISPLAY = False
@@ -38,23 +38,23 @@ class DefaultColor:
     CMD_FAILED_FG = 15
 
     SVN_CHANGES_BG = 148
-    SVN_CHANGES_FG = 22  # dark green
+    SVN_CHANGES_FG = 15  # dark green
 
     GIT_AHEAD_BG = 240
-    GIT_AHEAD_FG = 231
+    GIT_AHEAD_FG = 15
     GIT_BEHIND_BG = 240
-    GIT_BEHIND_FG = 231
+    GIT_BEHIND_FG = 15
     GIT_STAGED_BG = 22
-    GIT_STAGED_FG = 231
+    GIT_STAGED_FG = 15
     GIT_NOTSTAGED_BG = 130
-    GIT_NOTSTAGED_FG = 231
+    GIT_NOTSTAGED_FG = 15
     GIT_UNTRACKED_BG = 52
-    GIT_UNTRACKED_FG = 231
+    GIT_UNTRACKED_FG = 15
     GIT_CONFLICTED_BG = 9
-    GIT_CONFLICTED_FG = 231
+    GIT_CONFLICTED_FG = 15
 
     VIRTUAL_ENV_BG = 35  # a mid-tone green
-    VIRTUAL_ENV_FG = 00
+    VIRTUAL_ENV_FG = 15
 
 class Color(DefaultColor):
     """
