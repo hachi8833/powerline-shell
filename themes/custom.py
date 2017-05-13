b8bfc415bea02b8341fd380e4475ee8c3f5d651a
HATTA, Shozo <hachi8833@gmail.com>
2017-05-13 18:06:55 +0900

    Update colors

--- themes/custom.py
+++ themes/custom.py
@@ -25,9 +25,9 @@ class DefaultColor:
     SSH_FG = 254
 
     REPO_CLEAN_BG = 148  # a light green color
-    REPO_CLEAN_FG = 0  # black
+    REPO_CLEAN_FG = 231  # black
     REPO_DIRTY_BG = 161  # pink/red
-    REPO_DIRTY_FG = 15  # white
+    REPO_DIRTY_FG = 231  # white
 
     JOBS_FG = 39
     JOBS_BG = 238
